199dd7b7d0a15fa2554b1299b7cf264bef53ceaf
Evan Abbott <107959883+EvanA4@users.noreply.github.com>
2026-04-14 15:10:15 -0400

    fixed reproducibility

--- README.md
+++ README.md
@@ -1,17 +1,2 @@
 # BigDataClimateChange
 A Python-based approach to answer the following question: how have countries' daily temperature ranges changed over time?
-
-### How to Use
-**You must first download the necessary data!** NaturalEarthData contains [download links](https://www.naturalearthdata.com/downloads/110m-cultural-vectors/) to the necessary country data. Just download [this](https://naciscdn.org/naturalearth/110m/cultural/ne_110m_admin_0_countries.zip) country data ZIP file specifically. Extracting the ZIP file should give you the following files:
-
-```
-ne_110m_admin_0_countries.cpg
-ne_110m_admin_0_countries.prj
-ne_110m_admin_0_countries.shp
-ne_110m_admin_0_countries.VERSION.txt
-ne_110m_admin_0_countries.dbf
-ne_110m_admin_0_countries.README.html
-ne_110m_admin_0_countries.shx
-```
-
-Place `ne_110m_admin_0_countries.shp` into the root directory of your project.
--- utils.py
+++ utils.py
@@ -1,11 +1,23 @@
 import geopandas as gpd
 from shapely.geometry import Point
+import requests
+from zipfile import ZipFile
+from io import BytesIO
 
+def download_data():
+    url = 'https://naciscdn.org/naturalearth/110m/cultural/ne_110m_admin_0_countries.zip'
+    response = requests.get(url)
+    if response.status_code == 200:
+        with ZipFile(BytesIO(response.content)) as zip_file:
+            zip_file.extractall('mapdata')
+    else:
+        print(f"Failed to download file: {response.status_code}")
+        exit(1)
 
+download_data()
 WORLD_DATA = gpd.read_file(
-    "ne_110m_admin_0_countries.shp"
+    "mapdata/ne_110m_admin_0_countries.shp"
 )
-
 
 def get_country(lat, lon):
     point = Point(lon, lat)
@@ -17,12 +29,12 @@ def get_country(lat, lon):
 
 
 def main():
-    tests = list(map(lambda x: x[:-1].split(","), open("country_tests.csv").readlines()[1:]))
-    print(f"{"Expected":44s} | Guess")
+    tests = list(map(lambda x: x[:-1].split(","), open("mapdata/country_tests.csv").readlines()[1:]))
+    print(f"{'Expected':44s} | Guess")
     print("------------------+---------------------------------")
     for test in tests:
         guess = get_country(float(test[3]), float(test[2]))
-        print(f"{test[1]:44s} | {guess if guess is not None else ""}")
+        print(f"{test[1]:44s} | {guess if guess is not None else ''}")
 
 if __name__ == "__main__":
     main()
