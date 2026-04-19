9a899f90354f0727349d3ba3b05e93bd51aa5de8
RajasiG <exouser@image-build-instance-ubuntu-24-1767887635345636285.js2local>
2026-04-19 17:13:13 +0000

    improved utils

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # BigDataClimateChange
 A Python-based approach to answer the following question: how have countries' daily temperature ranges changed over time?
+
+### How To Run
+Download and extract all files from [this shapefile download](https://www.arcgis.com/home/item.html?id=fa510018bdd044b08fc64d2a16bc680a) into `/mapdata`. Alternatively, extract all files from [this download](https://www.arcgis.com/home/item.html?id=fa510018bdd044b08fc64d2a16bc680a) for a lower quality shapefile.
--- utils.py
+++ utils.py
@@ -3,29 +3,23 @@ from shapely.geometry import Point
 import requests
 from zipfile import ZipFile
 from io import BytesIO
+from pyproj import Transformer
+from unidecode import unidecode
 
-def download_data():
-    url = 'https://naciscdn.org/naturalearth/110m/cultural/ne_110m_admin_0_countries.zip'
-    response = requests.get(url)
-    if response.status_code == 200:
-        with ZipFile(BytesIO(response.content)) as zip_file:
-            zip_file.extractall('mapdata')
-    else:
-        print(f"Failed to download file: {response.status_code}")
-        exit(1)
-
-download_data()
-WORLD_DATA = gpd.read_file(
-    "mapdata/ne_110m_admin_0_countries.shp"
-)
-
-def get_country(lat, lon):
-    point = Point(lon, lat)
-    result = WORLD_DATA[WORLD_DATA.contains(point)]
-    if not result.empty:
-        return result.iloc[0]['NAME']
-    return None
-
+class CountryFinder:
+    def __init__(self):
+        self._transformer = Transformer.from_crs("EPSG:4326", "EPSG:3857", always_xy=True)
+        self._shapefile = gpd.read_file(
+            "mapdata/World_Countries.shp"
+        )
+        
+    def find(self, lat, lon):
+        wmcoords = self._transformer.transform(lon, lat)
+        point = Point(wmcoords[0], wmcoords[1])
+        result = self._shapefile[self._shapefile.contains(point)]
+        if not result.empty:
+            return unidecode(result.iloc[0]['COUNTRY'])
+        return None
 
 def main():
     tests = list(map(lambda x: x[:-1].split(","), open("mapdata/country_tests.csv").readlines()[1:]))
@@ -34,8 +28,10 @@ def main():
     nerr = 0
     nmiss = 0
     guesses = set()
+    cf = CountryFinder()
     for test in tests:
-        guess = get_country(float(test[3]), float(test[2]))
+        test[1] = unidecode(test[1])
+        guess = cf.find(float(test[3]), float(test[2]))
         if guess not in guesses: guesses.add(guess)
         print(f"{test[1]:44s} | {guess if guess is not None else ''}")
         if not guess:
